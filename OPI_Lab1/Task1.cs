83720caec93b43544594feb5ca111182ab9919f8
X3ros7 <a.a.prosv2005@gmail.com>
2024-09-02 13:42:05 +0300

    Add project files.

---
+++ OPI_Lab1/Task1.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name;
+        int age;
+        int num;
+
+        Console.WriteLine("Enter your name: ");
+        name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}! Now, enter your age");
+        age = int.Parse(Console.ReadLine());
+        if (age <= 0)
+        {
+            Console.WriteLine("Error! Age can`t be negative or 0");
+            return;
+        }
+
+        Console.WriteLine($"You'll reach 100 years old in {2024 - age + 100}");
+        Console.WriteLine("Now enter a random inreger number");
+
+        num = int.Parse(Console.ReadLine());
+        for (int i = 0; i < num; i++)
+        {
+            Console.WriteLine($"You'll reach 100 years old in {2024 - age + 100}");
+        }
+    }
+}
---
+++ Task2/Task2.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int first_num;
+        int second_num;
+
+        Console.WriteLine("Enter a number");
+        first_num = int.Parse(Console.ReadLine());
+        if (first_num % 2 == 0)
+        {
+            Console.WriteLine("This number is even");
+        }
+        else
+        {
+            Console.WriteLine("This number is odd");
+        }
+
+        if (first_num % 4 == 0)
+        {
+            Console.WriteLine("This number is divisible by 4");
+        }
+
+        Console.WriteLine("Now enter another number");
+        second_num = int.Parse(Console.ReadLine());
+        if (first_num % second_num == 0 || second_num % first_num == 0)
+        {
+            Console.WriteLine($"Number {first_num} and {second_num} are divisible");
+        }
+        else
+        {
+            Console.WriteLine($"Number {first_num} and {second_num} are not divisible");
+        }
+    }
+}
